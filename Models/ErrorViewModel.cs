c3ca9cfe0918f9f4a009101e526330de16dcd82e
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-04 19:53:25 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TreehouseFitnessFrog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
